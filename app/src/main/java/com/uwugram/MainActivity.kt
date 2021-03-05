de9e6776516919d82885b25042362921671ff23e
luchkalex <tedy5ruzvelt@gmail.com>
2021-03-05 14:47:22 +0200

    Initial commit

---
+++ app/src/main/java/com/uwugram/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.uwugram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.Drawer
+import com.uwugram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
