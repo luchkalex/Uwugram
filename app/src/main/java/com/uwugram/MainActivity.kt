45685bb2f1f29fc2496fc647ca4d0c16ff204e00
luchkalex <tedy5ruzvelt@gmail.com>
2021-09-11 17:22:43 +0300

    Binding dependencies added

--- app/src/main/java/com/uwugram/MainActivity.kt
+++ app/src/main/java/com/uwugram/MainActivity.kt
@@ -2,9 +2,14 @@ package com.uwugram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.uwugram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
