09a9eaee42ed6bc5532a25da93537319b44eb5d7
luchkalex <tedy5ruzvelt@gmail.com>
2021-09-14 17:55:12 +0300

    Add menu to settings fragment

--- app/src/main/java/com/uwugram/view/fragments/SettingsFragment.kt
+++ app/src/main/java/com/uwugram/view/fragments/SettingsFragment.kt
@@ -1,5 +1,18 @@
 package com.uwugram.view.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.uwugram.R
 
-class SettingsFragment : AbstractFragment(R.layout.fragment_settings)
+class SettingsFragment : AbstractFragment(R.layout.fragment_settings){
+
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        activity?.menuInflater?.inflate(R.menu.settings_menu, menu)
+    }
+}
