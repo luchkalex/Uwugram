0fc1cfaf0c4a7bafdff174e6b9a9ee0b8c598013
luchkalex <tedy5ruzvelt@gmail.com>
2021-09-20 20:37:14 +0300

    Feat: Add logout

--- app/src/main/java/com/uwugram/view/fragments/SettingsFragment.kt
+++ app/src/main/java/com/uwugram/view/fragments/SettingsFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import com.uwugram.R
+import com.uwugram.activities.LoginActivity
+import com.uwugram.activities.MainActivity
 import com.uwugram.databinding.FragmentSettingsBinding
+import com.uwugram.utils.AUTH
+import com.uwugram.utils.replaceActivity
 
 class SettingsFragment : Fragment() {
 
@@ -29,5 +33,15 @@ class SettingsFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         activity?.menuInflater?.inflate(R.menu.settings_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_menu_logout -> {
+                AUTH.signOut()
+                (activity as MainActivity).replaceActivity(LoginActivity())
+            }
+        }
+        return true
     }
 }
